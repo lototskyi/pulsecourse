cde88382d5c9daf379c449b5f2fcf8ac557a495e
alex <o.lototskyi@gmail.com>
2022-09-01 19:41:30 +0300

    added modal dialogs

--- src/js/script.js
+++ src/js/script.js
@@ -33,4 +33,22 @@ $(document).ready(function(){
 
     togleClass('.catalog-item__link');
     togleClass('.catalog-item__back');
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn();
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, .modal').fadeOut();
+    });
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn();
+        });
+    });
+    $('.overlay').on('click', function({target}) {
+        if (target.classList.contains('overlay')) {
+            $('.overlay, .modal').fadeOut();
+        }
+    })
 });
